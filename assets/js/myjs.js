5aac9a3cead40673a10ee374e449a75219d67452
acoleshop <111170283+acoleshop@users.noreply.github.com>
2022-08-13 00:17:45 -0600

    Add files via upload

---
+++ assets/js/myjs.js
@@ -0,0 +1,36 @@
+let _n1 = 10;
+let _n2 = 2;
+
+let _suma = _n1 + _n2;
+// Resultado: 8
+
+let _resta = _n1 - _n2;
+// Resultado: 12
+
+let _multiplicacion = _n1 * _n2;
+// Resultado: 20
+
+let _division = _n1 / _n2;
+// Resultado: 5
+
+
+let _numero1 = 1;
+let _numero2 = 2.0;
+
+let _string1 = "Cadena 1";
+let _string2 = 'Cadena 2';
+
+
+let _string3 = `Cadena combinada: ${_string1} ${_string2}`;
+
+//Forma de comentar numero 1
+segunda linea de codigo
+
+
+/* Forma de comentar 2
+segunda linea de codigo
+*/
+
+
+let _nombreCompleto = '';
+let _NombreCompleto = '';
